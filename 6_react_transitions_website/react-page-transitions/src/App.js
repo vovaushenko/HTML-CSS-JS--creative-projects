8f040fa492e810707d0af453fc6847c6e2168345
vovaushenko <vovaushenko1989@gmail.com>
2020-12-18 11:26:03 +0200

    upd

--- 6_react_transitions_website/react-page-transitions/src/App.js
+++ 6_react_transitions_website/react-page-transitions/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import { Switch, Route, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import { AnimatePresence } from 'framer-motion';
-import GlobalStyle from './globalStyles';
-import styled from 'styled-components';
+import "./App.css";
+import { Switch, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./globalStyles";
+import styled from "styled-components";
 
 const Section = styled.section`
   overflow-x: hidden;
